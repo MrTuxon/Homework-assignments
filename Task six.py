6f8a7ca2c2fc07fb8cea0d3920ccbef16ccaead0
Nikolay <119748239+MrTuxon@users.noreply.github.com>
2022-12-16 23:07:39 +0500

    Add files via upload

---
+++ Task six.py
@@ -0,0 +1,9 @@
+print('Code running')
+print('A program for calculating the volume and area of its total surface.')
+print('Rib length 26.')
+A = int(25)
+B = A ** 3
+Z = A ** 2 * 6
+print('Result')
+print('Volume =', B)
+print('Total surface area =', Z)
